b62b0b61c7b3cfbd654d2d019c7aa6ff8d618235
Diego Viana <diegovianaf7@gmail.com>
2022-09-20 16:04:51 -0300

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Header from "./components/Header"
+import Home from "./pages/Home"
+import Users from "./pages/Users"
+import Page404 from "./pages/Page404"
 
 function App() {
   return (
-    <div>
-      
-    </div>
-  );
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="users" element={<Users />} />
+        <Route path="*" element={<Page404 />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
